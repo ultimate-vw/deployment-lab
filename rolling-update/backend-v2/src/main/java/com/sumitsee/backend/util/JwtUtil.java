b4e306f899f0ce825ea3d2eddc67007f800104c2
Sum+it <sumitkamble35@gmail.com>
2025-08-02 22:39:21 +0530

    ADD: Rolling update Kubernetes setup complete

---
+++ rolling-update/backend-v2/src/main/java/com/sumitsee/backend/util/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.sumitsee.backend.util;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    private final long JWT_EXPIRATION = 24 * 60 * 60 * 1000; // 24 hours
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
