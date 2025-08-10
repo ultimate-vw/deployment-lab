8447a8bc130021d6dfa0f571562095ff1152e6a8
Sum+it <sumitkamble35@gmail.com>
2025-08-10 21:33:04 +0530

    UPDATE: Adding backend A for the ab testing setup

---
+++ ab-testing/backend-v1/src/main/java/com/sumitsee/backend/controller/VersionController.java
@@ -0,0 +1,15 @@
+package com.sumitsee.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+    @GetMapping("/api/versions")
+    public String getVersion() {
+        return "Hi! I am backend A!";
+    }
+
+}
+
